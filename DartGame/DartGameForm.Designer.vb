714ce60983f28618e7d5f3110f5f02bfd2733f23
Jason Permann <JaceFoxman@gmail.com>
2025-10-16 15:30:01 -0600

    ADD Dart Game Code

--- DartGame/DartGameForm.Designer.vb
+++ DartGame/DartGameForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class DartGameForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,19 +20,116 @@ Partial Class DartGameForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.Clear_Button = New System.Windows.Forms.Button()
+        Me.Quit_Button = New System.Windows.Forms.Button()
+        Me.Drawing_PictureBox = New System.Windows.Forms.PictureBox()
+        Me.ThrowDart_Button = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        CType(Me.Drawing_PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'Clear_Button
+        '
+        Me.Clear_Button.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Clear_Button.ForeColor = System.Drawing.Color.Cornsilk
+        Me.Clear_Button.Location = New System.Drawing.Point(571, 424)
+        Me.Clear_Button.Name = "Clear_Button"
+        Me.Clear_Button.Size = New System.Drawing.Size(241, 133)
+        Me.Clear_Button.TabIndex = 1
+        Me.Clear_Button.Text = "Clear Board"
+        Me.Clear_Button.UseVisualStyleBackColor = False
+        '
+        'Quit_Button
+        '
+        Me.Quit_Button.BackColor = System.Drawing.Color.DarkRed
+        Me.Quit_Button.ForeColor = System.Drawing.Color.Cornsilk
+        Me.Quit_Button.Location = New System.Drawing.Point(818, 424)
+        Me.Quit_Button.Name = "Quit_Button"
+        Me.Quit_Button.Size = New System.Drawing.Size(241, 133)
+        Me.Quit_Button.TabIndex = 2
+        Me.Quit_Button.Text = "Quit Game"
+        Me.Quit_Button.UseVisualStyleBackColor = False
+        '
+        'Drawing_PictureBox
+        '
+        Me.Drawing_PictureBox.Location = New System.Drawing.Point(17, 6)
+        Me.Drawing_PictureBox.Name = "Drawing_PictureBox"
+        Me.Drawing_PictureBox.Size = New System.Drawing.Size(1042, 412)
+        Me.Drawing_PictureBox.TabIndex = 3
+        Me.Drawing_PictureBox.TabStop = False
+        '
+        'ThrowDart_Button
+        '
+        Me.ThrowDart_Button.BackColor = System.Drawing.Color.ForestGreen
+        Me.ThrowDart_Button.ForeColor = System.Drawing.Color.Cornsilk
+        Me.ThrowDart_Button.Location = New System.Drawing.Point(17, 424)
+        Me.ThrowDart_Button.Name = "ThrowDart_Button"
+        Me.ThrowDart_Button.Size = New System.Drawing.Size(241, 133)
+        Me.ThrowDart_Button.TabIndex = 0
+        Me.ThrowDart_Button.Text = "Throw Dart"
+        Me.ThrowDart_Button.UseVisualStyleBackColor = False
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Appearance = System.Windows.Forms.TabAppearance.Buttons
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1084, 604)
+        Me.TabControl1.TabIndex = 3
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.ThrowDart_Button)
+        Me.TabPage1.Controls.Add(Me.Quit_Button)
+        Me.TabPage1.Controls.Add(Me.Clear_Button)
+        Me.TabPage1.Controls.Add(Me.Drawing_PictureBox)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 32)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1076, 568)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 32)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1076, 568)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'DartGameForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1101, 623)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "DartGameForm"
         Me.Text = "DartGame"
+        CType(Me.Drawing_PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents Clear_Button As Button
+    Friend WithEvents Quit_Button As Button
+    Friend WithEvents Drawing_PictureBox As PictureBox
+    Friend WithEvents ThrowDart_Button As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
 End Class
--- DartGame/DartGameForm.vb
+++ DartGame/DartGameForm.vb
@@ -7,8 +7,120 @@
 Option Strict On
 Option Explicit On
 Option Compare Text
-Public Class DartGameForm
-    Private Sub DartGameForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+Public Class DartGameForm
+    Public Round As Integer
+    Public roundString As String
+    Private Sub DartGameForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetDefaults()
+        RoundTrack()
+    End Sub
+
+    Sub SetDefaults()
+        'clear the drawing area
+        Drawing_PictureBox.Refresh()
+        Drawing_PictureBox.BackColor = Color.AliceBlue
+        Drawing_PictureBox.BorderStyle = BorderStyle.Fixed3D
+    End Sub
+
+    Sub DrawDart(x%, y%)
+        Dim g As Graphics = Drawing_PictureBox.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+        Dim dartWidth% = 30
+
+        g.DrawEllipse(pen, x - (dartWidth \ 2), y - (dartWidth \ 2), dartWidth, dartWidth)
+        g.DrawLine(pen, x - 3, y, x + 3, y)
+        g.DrawLine(pen, x, y - 3, x, y + 3)
+
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Sub AskToStartNewRound()
+        Dim result As DialogResult
+        result = MessageBox.Show("Thats 3 Darts, Do you want to start a new round?", "New Round", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.Yes Then
+            SetDefaults()
+            RoundTrack()
+
+        Else
+            SetDefaults()
+            RoundTrack()
+
+        End If
+    End Sub
+
+    Function DartTrack() As Integer
+        Static count As Integer = 0 'Static variable to hold count value
+        count += 1 'Increase count
+        If count = 3 Then
+            AskToStartNewRound()
+            count = 0 'Reset count
+
+        End If
+        Return count    'Return count value
+    End Function
+
+    Function RoundTrack() As Integer
+        Static roundCount As Integer = 0 'Static variable to hold round count value
+        roundCount += 1 'Increase round count    'Return round count value
+        Return roundCount
+    End Function
+
+    Sub ThrowDart()
+        Dim path As String = "DartGame.log"
+        Dim X As Integer = GetRandomNumberZeroTo(Drawing_PictureBox.Width)
+        Dim Y As Integer = GetRandomNumberZeroTo(Drawing_PictureBox.Height)
+        DrawDart(X, Y)
+
+
+        FileOpen(1, path, OpenMode.Append)
+        PrintLine(1, $"X Coordinate: {X}" & $" Y Coordinate: {Y}")
+        FileClose(1)
+
+        DartTrack()
+
+    End Sub
+
+    Function GetRandomNumberZeroTo(max%) As Integer
+        Dim result As Integer
+        Randomize()
+        result = CInt(System.Math.Floor(CDbl(Rnd() * max))) + 1
+        'record the random number generated
+        Return result
+    End Function
+
+    Private Sub ThrowDart_Button_Click(sender As Object, e As EventArgs) Handles ThrowDart_Button.Click
+        Dim path As String = "DartGame.log"
+
+        Round += 1 'RoundCountToString()
+        roundString = Round.ToString
+        FileOpen(1, path, OpenMode.Append)
+        PrintLine(1, "-------------------")
+        WriteLine(1, $"Round: {roundString}")
+        FileClose(1)
+        ThrowDart()
+        ThrowDart()
+        ThrowDart()
+    End Sub
+    Private Sub Clear_Button_Click(sender As Object, e As EventArgs) Handles Clear_Button.Click
+        SetDefaults()
+        RoundTrack()
+
+    End Sub
+    Private Sub TabPage2_Click(sender As Object, e As EventArgs) Handles TabPage2.Click
+        SetDefaults()
+
+        RoundTrack()
+        ThrowDart_Button.Enabled = False
+    End Sub
+    Private Sub TabPage1_Click(sender As Object, e As EventArgs) Handles TabPage1.Click
+        SetDefaults()
+
+        RoundTrack()
+        ThrowDart_Button.Enabled = True
+    End Sub
+    Private Sub Quit_Button_Click(sender As Object, e As EventArgs) Handles Quit_Button.Click
+        Me.Close()
     End Sub
 End Class
